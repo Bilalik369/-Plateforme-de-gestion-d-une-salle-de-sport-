1d559887401afe0a70eb5faae1c9c864c8f0f57b
Bilalik369 <bilal11iken@gmail.com>
2025-03-11 12:17:40 +0000

    Ajout du modèle utilisateur avec Mongoose

---
+++ backend/models/User.js
@@ -0,0 +1,17 @@
+
+const mongoose= require('mongoose');
+
+
+
+const UserSchema = new mongoose.Schema({
+
+    name : {type: String , required: true},
+    email :{type : String , required: true},
+    password :{type:String , required: true},
+    role :{type:String , required:true, enum: ["membre" , "trainer"]},
+
+    
+});
+
+
+module.exports= mongoose.model('User' , UserSchema);
